db6d794b22d37fdfea8ed39e13548583413a4de1
Deveshi-ds <deveshi.dj20@gmail.com>
2024-12-06 17:21:23 +0530

    Add files via upload

---
+++ RESULTS_cred-transac,py.py
@@ -0,0 +1,24 @@
+
+Epoch [1/10], Loss: 0.0357
+Epoch [2/10], Loss: 0.0208
+Epoch [3/10], Loss: 0.0206
+Epoch [4/10], Loss: 0.0206
+Epoch [5/10], Loss: 0.0208
+Epoch [6/10], Loss: 0.0209
+Epoch [7/10], Loss: 0.0207
+Epoch [8/10], Loss: 0.0208
+Epoch [9/10], Loss: 0.0207
+Epoch [10/10], Loss: 0.0208
+Training complete. Model is ready for embeddings.
+Best Threshold: 0.0603
+Improved ROC-AUC Score: 0.9076
+Improved Precision: 0.0226, Recall: 0.6548, F1-Score: 0.0438
+Sample of Improved Anomalous Transactions Detected:
+            Time        V1        V2  ...       V27       V28    Amount
+165061  0.678116  0.951482  0.760643  ...  0.409673  0.310186  0.001787
+189362  0.742870  0.960323  0.767542  ...  0.415597  0.309224  0.001724
+66915   0.302531  0.893507  0.717483  ...  0.417383  0.297864  0.000584
+82956   0.345074  0.969751  0.755786  ...  0.418437  0.313600  0.004749
+59897   0.283914  0.970774  0.760626  ...  0.419271  0.313834  0.002162
+
+[5 rows x 30 columns]
